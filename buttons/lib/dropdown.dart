fe29a9d7a424bd6d3ce164a06b8209919ea96dcd
pranave2003 <pranav2003a10@gmail.com>
2023-10-31 20:31:53 +0530

    4th

--- buttons/lib/dropdown.dart
+++ buttons/lib/dropdown.dart
@@ -1,18 +1,50 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Dropdowmss extends StatefulWidget {
-  const Dropdowmss({super.key});
+
+class Drop extends StatefulWidget {
+  const Drop({super.key});
 
   @override
-  State<Dropdowmss> createState() => _DropdowmssState();
+  State<Drop> createState() => _DropState();
 }
 
-class _DropdowmssState extends State<Dropdowmss> {
+class _DropState extends State<Drop> {
+  String dropdownvalue = 'Item 1';
+
+  var items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    "data",
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.black54),
+    return Scaffold(backgroundColor: Colors.yellow,
+      body: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          DropdownButton(
+
+            value: dropdownvalue,
+
+            icon: const Icon(Icons.keyboard_arrow_down),
+
+            items: items.map((String items) {
+              return DropdownMenuItem(
+                value: items,
+                child: Text(items),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownvalue = newValue!;
+              });
+            },
+          ),
+          //  Cb(text: '')
+        ],
+      ),
     );
   }
 }
---
+++ buttons/lib/navigation buton.dart
@@ -0,0 +1,65 @@
+import 'package:buttons/A%20bag.dart';
+import 'package:buttons/B%20bag.dart';
+import 'package:flutter/material.dart';
+
+
+import 'dropdown.dart';
+
+class MyNavigationBar extends StatefulWidget {
+  MyNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  _MyNavigationBarState createState() => _MyNavigationBarState();
+}
+
+class _MyNavigationBarState extends State<MyNavigationBar> {
+
+  int _selectedIndex = 0;
+
+
+
+  static const List<dynamic> _widgetOptions = [
+    Text('My home page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+   Abag(),
+
+    Bbag()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+                backgroundColor: Colors.blue),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_bag_rounded),
+                label: 'Abag',
+                backgroundColor: Colors.blue),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.badge),
+                label: 'Bbag',
+                backgroundColor: Colors.blue),
+
+          ],
+          type: BottomNavigationBarType.shifting,
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.black45,
+          iconSize: 40,
+          onTap: _onItemTapped,
+          elevation: 5),
+    );
+  }
+}
