f219ca263f567f53807635b39d5a7188d25d8000
pranave2003 <pranav2003a10@gmail.com>
2023-12-03 16:31:23 +0530

    1st

---
+++ logicalshape/lib/CountFrequancyNumber.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(){
+ print("enter number");
+ var n=stdin.readLineSync();
+
+ for(int i=0;i<=n;i++);
+
+
+
+}
--- logicalshape/lib/powernof a number.dart
+++ logicalshape/lib/powernof a number.dart
@@ -1,16 +1,16 @@
 import 'dart:io';
 void main(){
-  int c=1,j,K;
+  int c=1,j=1,K;
   print("ENTER A NUMBER");
   var a=stdin.readLineSync();
  int d= int.parse(a!);
  print("enter 2nd num");
  var b=stdin.readLineSync();
  int e=int.parse(b!);
- for(int c=1;c<=e;c++){
-int j=c*d;
-print("$j");
-   }
+ for(int i=1;i<=e;i++){
+j=j*d;
 
+   }
+ print("$j");
 
 }
